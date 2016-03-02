f58d5f795940be9ebefe97864fa70324a2d4d072
gdevailly <gdevailly@hotmail.com>
2016-03-02 10:12:34 +0000

    new public url

--- application/heatstarseq/data/server_adresses.R
+++ application/heatstarseq/data/server_adresses.R
@@ -1,4 +1,4 @@
-URL_HEATSTARSEQ <- "http://www.chipcompare.roslin.ed.ac.uk"
-URL_HEATRNASEQ <- "http://www.chipcompare.roslin.ed.ac.uk/heatrnaseq"
-URL_HEATCHIPSEQ <- "http://www.chipcompare.roslin.ed.ac.uk/heatchipseq"
-URL_HEATCAGESEQ <- "http://www.chipcompare.roslin.ed.ac.uk/heatcageseq"
+URL_HEATSTARSEQ <- "http://www.heatstarseq.roslin.ed.ac.uk"
+URL_HEATRNASEQ <- "http://www.heatstarseq.roslin.ed.ac.uk/heatrnaseq"
+URL_HEATCHIPSEQ <- "http://www.heatstarseq.roslin.ed.ac.uk/heatchipseq"
+URL_HEATCAGESEQ <- "http://www.heatstarseq.roslin.ed.ac.uk/heatcageseq"
